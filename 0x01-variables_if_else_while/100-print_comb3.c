1ba2a2f4f8bd720e414f14832db499f375670e72
MoeJoe03 <oneeyed211@gmail.com>
2023-03-17 17:18:57 -0700

    print comb advanced

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of two digits
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; j < 10 ; j++)
+	{
+		for (j = 1 ; j < 10 ; j++)
+		{
+			if (i < j && i != j)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				if (i + j != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
