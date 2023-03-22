0649f5d64514e99e10a0ab4eec3e2e6cae91c2d4
MoeJoe03 <oneeyed211@gmail.com>
2023-03-21 17:01:38 -0700

    3-islower

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,7 +4,7 @@
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int islower(int c);
+int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
@@ -13,6 +13,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void print_table(int n);
+void print_times_table(int n);
 
 #endif
